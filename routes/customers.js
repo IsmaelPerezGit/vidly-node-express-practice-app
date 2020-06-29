4ed19eb208d849cab3e12a23a11a79d7c52bbf5c
Ismael <IsmaelPerezEmail@gmail.com>
2020-06-29 15:10:28 -0700

    chore(App): basic set up of customer routes complete

--- routes/customers.js
+++ routes/customers.js
@@ -12,6 +12,12 @@ const Customer = mongoose.model(
       minlength: 1,
       maxlength: 50,
     },
+    phone: {
+      type: String,
+      required: true,
+      minlength: 10,
+      maxlength: 10,
+    },
   })
 );
 
@@ -20,13 +26,50 @@ router.get('/', async (req, res) => {
   res.send(customers);
 });
 
+router.get('/:id', async (req, res) => {
+  const customer = await Customer.findById(req.params.id);
+  if (!customer) res.status(404).send('A customer with the provided id was not found...');
+  res.send(customer);
+});
+
+router.post('/', async (req, res) => {
+  const { error } = validateCustomer(req.body);
+  if (error) res.status(400).send(error.details[0].message);
+  let customer = new Customer({
+    name: req.body.name,
+    phone: req.body.phone,
+  });
+  customer = await customer.save();
+  res.send(customer);
+});
+
+router.put('/:id', async (req, res) => {
+  const { error } = validateCustomer(req.body);
+  if (error) res.status(400).send(error.details[0].message);
+  const customer = await Customer.findByIdAndUpdate(
+    req.params.id,
+    {
+      name: req.body.name,
+      phone: req.body.phone,
+    },
+    { new: true }
+  );
+  if (!customer) res.status(404).send('A customer with the given id was not found...');
+  res.send(customer);
+});
+
+router.delete('/:id', async (req, res) => {
+  const customer = await Customer.findByIdAndRemove(req.params.id);
+  if (!customer) res.status(404).send('A customer with the given id was not found...');
+  res.send(customer);
+});
+
 const validateCustomer = customer => {
   const schema = {
     name: Joi.string().min(1).required(),
     phone: Joi.required(),
   };
-
-  return Joi.validate(genre, schema);
+  return Joi.validate(customer, schema);
 };
 
 module.exports = router;
