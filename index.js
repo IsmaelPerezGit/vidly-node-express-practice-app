8ab27185d95a2315d5aed8366dbce2a046f5fefd
Ismael <IsmaelPerezEmail@gmail.com>
2020-06-29 10:33:49 -0700

    chore(App): added post route for adding genres and created a basic validation function using Joi

--- index.js
+++ index.js
@@ -14,5 +14,25 @@ app.get('/api/genres', (req, res) => {
   res.send(genres);
 });
 
+app.post('/api/genres', (req, res) => {
+  const { error } = validateGenre(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const genre = {
+    id: genres.length + 1,
+    name: req.body.name,
+  };
+  genres.push(genre);
+  res.send(genre);
+});
+
+const validateGenre = genre => {
+  const schema = {
+    name: Joi.string().min(3).required(),
+  };
+
+  return Joi.validate(genre, schema);
+};
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Now listening on port ${port}`));
