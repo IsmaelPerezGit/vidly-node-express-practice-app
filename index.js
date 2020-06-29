0625fff3eca51d49ecfa4cf512f1bb71d6f7c048
Ismael <IsmaelPerezEmail@gmail.com>
2020-06-29 10:27:33 -0700

    chore(App): set up mock genres data and get genres route

--- index.js
+++ index.js
@@ -4,7 +4,15 @@ const app = express();
 
 app.use(express.json());
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  `Now listening on port ${port}`;
+const genres = [
+  { id: 1, name: 'Action' },
+  { id: 2, name: 'Horror' },
+  { id: 3, name: 'Romance' },
+];
+
+app.get('/api/genres', (req, res) => {
+  res.send(genres);
 });
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Now listening on port ${port}`));
