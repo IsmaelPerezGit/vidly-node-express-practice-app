21a5e4146ec3dafbad2b8eea7c6a4d17736ac570
Ismael <IsmaelPerezEmail@gmail.com>
2020-06-29 15:43:51 -0700

    chore(App): moved genre model code into its own file

--- routes/genres.js
+++ routes/genres.js
@@ -1,19 +1,6 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const Joi = require('joi');
 const router = express.Router();
-
-const Genre = mongoose.model(
-  'Genre',
-  new mongoose.Schema({
-    name: {
-      type: String,
-      required: true,
-      minlength: 5,
-      maxlength: 50,
-    },
-  })
-);
+const { Genre, validateGenre } = require('./models/genre');
 
 router.get('/', async (req, res) => {
   const genres = await Genre.find().sort('name');
@@ -51,13 +38,5 @@ router.get('/:id', async (req, res) => {
   if (!genre) res.status(404).send('The genre with the given id was not found...');
   res.send(genre);
 });
-
-const validateGenre = genre => {
-  const schema = {
-    name: Joi.string().min(3).required(),
-  };
-
-  return Joi.validate(genre, schema);
-};
 
 module.exports = router;
