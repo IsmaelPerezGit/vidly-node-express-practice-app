590417be73b4340ff3582655721f61782a1036fb
Ismael <IsmaelPerezEmail@gmail.com>
2020-06-29 13:11:06 -0700

    chore(App): created mongoose model and added genre schema directly to model for cleaner code

--- routes/genres.js
+++ routes/genres.js
@@ -2,14 +2,17 @@ const express = require('express');
 const mongoose = require('mongoose');
 const router = express.Router();
 
-const genreSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    minlength: 5,
-    maxlength: 50,
-  },
-});
+const Genre = new mongoose.model(
+  'Genre',
+  new mongoose.Schema({
+    name: {
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 50,
+    },
+  })
+);
 
 const genres = [
   { id: 1, name: 'Action' },
