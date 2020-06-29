4e9b94b7a3e8393db498abb304ff2e9b47ab5c63
Ismael <IsmaelPerezEmail@gmail.com>
2020-06-29 13:03:51 -0700

    chore(App): connected to mongoDB

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const Joi = require('joi');
 const express = require('express');
+const mongoose = require('mongoose');
+const mongoPass = require('./keys');
 const genres = require('./routes/genres');
 const app = express();
+
+const mongoConnectionUrl = `mongodb+srv://admin:${mongoPass}@cluster0-iikki.mongodb.net/vidly-practice?retryWrites=true&w=majority`;
+
+mongoose
+  .connect(mongoConnectionUrl)
+  .then(() => console.log('CONNECTED TO MONGO DB...'))
+  .catch(() => console.error('COULD NOT ESTABLISH CONNECTION'));
 
 app.use(express.json());
 app.use('/api/genres', genres);
