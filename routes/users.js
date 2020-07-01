9e296372e1c94dc07d87f9b76e56e09cf7bf0ddc
Ismael <IsmaelPerezEmail@gmail.com>
2020-07-01 09:56:34 -0700

    chore(App): added lodash library and integrated it into users routes

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const _ = require('lodash');
 const { User, validateUser } = require('../models/user');
 
 router.post('/', async (req, res) => {
@@ -8,14 +9,10 @@ router.post('/', async (req, res) => {
   let user = await User.findOne({ email: req.body.email });
   if (user) res.status(400).send('User already registered');
 
-  user = new User({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-  });
+  user = new User(_.pick(req.body, ['name', 'email', 'password']));
 
   await user.save();
-  res.send(user);
+  res.send(_.pick(user, ['_id', 'name', 'email']));
 });
 
 module.exports = router;
