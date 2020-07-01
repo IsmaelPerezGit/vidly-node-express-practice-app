3446fd057d0964dae63a307dc3904b4e3a01d23f
Ismael <IsmaelPerezEmail@gmail.com>
2020-07-01 11:46:01 -0700

    chore(App): now generating token, and setting it as a header in register user request

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,5 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const express = require('express');
 const router = express.Router();
 const _ = require('lodash');
@@ -9,13 +11,12 @@ router.post('/', async (req, res) => {
   if (error) res.status(400).send(error.details[0].message);
   let user = await User.findOne({ email: req.body.email });
   if (user) res.status(400).send('User already registered');
-
   user = new User(_.pick(req.body, ['name', 'email', 'password']));
   const salt = await bcrypt.genSalt(10);
   user.password = await bcrypt.hash(user.password, salt);
-
   await user.save();
-  res.send(_.pick(user, ['_id', 'name', 'email']));
+  const token = jwt.sign({ _id: user._id }, config.get('jwtPrivateKey'));
+  res.header('-x-auth-token', token).send(_.pick(user, ['_id', 'name', 'email']));
 });
 
 module.exports = router;
