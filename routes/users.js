be1a2239770c2bdb82a8b1a3e75c4d3fcf7ed801
Ismael <IsmaelPerezEmail@gmail.com>
2020-07-01 13:43:23 -0700

    chore(App): added route for getting individual user using jwt

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,16 @@
 const jwt = require('jsonwebtoken');
 const config = require('config');
+const auth = require('../middleware/auth');
 const express = require('express');
 const router = express.Router();
 const _ = require('lodash');
 const bcrypt = require('bcrypt');
 const { User, validateUser } = require('../models/user');
+
+router.get('/me', auth, async (req, res) => {
+  const user = await User.findById(req.user._id).select('-password');
+  res.send(user);
+});
 
 router.post('/', async (req, res) => {
   const { error } = validateUser(req.body);
