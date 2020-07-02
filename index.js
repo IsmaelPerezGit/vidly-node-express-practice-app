9542e41d5b1e456dc175fae0db769376ea4c8e7c
Ismael <IsmaelPerezEmail@gmail.com>
2020-07-02 13:54:18 -0700

    chore(App): moved code related to routes to its own file

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require('express-async-errors');
 require('winston-mongodb');
 const winston = require('winston');
-const error = require('./middleware/error');
 const config = require('config');
 const express = require('express');
 const mongoose = require('mongoose');
@@ -9,12 +8,7 @@ const mongoPass = require('./keys');
 const app = express();
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
-const genres = require('./routes/genres');
-const customers = require('./routes/customers');
-const movies = require('./routes/movies');
-const rentals = require('./routes/rentals');
-const users = require('./routes/users');
-const auth = require('./routes/auth');
+require('./startup/routes')(app);
 
 const mongoConnectionUrl = `mongodb+srv://admin:${mongoPass}@cluster0-iikki.mongodb.net/vidly-practice?retryWrites=true&w=majority`;
 
@@ -48,15 +42,6 @@ mongoose
   .connect(mongoConnectionUrl)
   .then(() => console.log('CONNECTED TO MONGO DB...'))
   .catch(() => console.error('COULD NOT ESTABLISH CONNECTION'));
-
-app.use(express.json());
-app.use('/api/genres', genres);
-app.use('/api/customers', customers);
-app.use('/api/movies', movies);
-app.use('/api/rentals', rentals);
-app.use('/api/users', users);
-app.use('/api/auth', auth);
-app.use(error);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Now listening on port ${port}`));
