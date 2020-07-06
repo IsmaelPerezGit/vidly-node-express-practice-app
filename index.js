f1b73a8c00ee35c9d1d3915d6e83b2fff49d6f3c
Ismael <IsmaelPerezEmail@gmail.com>
2020-07-06 10:25:21 -0700

    chore(App): now invoking loggin and startup functions

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 const winston = require('winston');
 
-require('./startup/logging');
+require('./startup/logging')();
 require('./startup/routes')(app);
 require('./startup/db')();
 require('./startup/config')();
-require('./startup/validation');
+require('./startup/validation')();
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => winston.info(`Now listening on port ${port}`));
